d226671db452ca6262c77854641f8cc1f8ed4e51
First Last <salmanashraf853@gmail.com>
2019-07-19 04:04:58 +0100

    building the container

---
+++ src/components/container/Comtainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './container.css'
+
+const Container = () => {
+    return (
+        <div className="container">
+            
+        </div>
+    )
+}
+
+export default Container
