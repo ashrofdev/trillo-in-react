f5629f41d82c8bb921eb398c14b46baabde520e6
First Last <salmanashraf853@gmail.com>
2019-07-19 04:36:06 +0100

    building the container

--- src/components/container/Comtainer.js
+++ src/components/container/Comtainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './container.css'
+import Header from '../header/Header'
 
 const Container = () => {
     return (
         <div className="container">
-            
+            <Header/>
         </div>
     )
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () =>{
+    return (
+        <header>
+            <div className="logo">logo</div>
+            <div className="search-box">
+                <input placeholder="search hotels"/>
+            </div>
+            <div className="icons">icons</div>
+        </header>
+    )
+}
+export default Header
