9b6b2d7b0d776931843143371b854444edf6dc09
First Last <salmanashraf853@gmail.com>
2019-07-21 17:02:34 +0100

    building the cta section

--- src/components/Hotel-view/HotelView.js
+++ src/components/Hotel-view/HotelView.js
@@ -3,9 +3,9 @@ import React from 'react'
 const HotelView = () => {
     return (
         <div className="hotel-view">
-            <img src={ require('./img/hotel-1.jpg') }/>
-            <img src={ require('./img/hotel-2.jpg') }/>
-            <img src={ require('./img/hotel-3.jpg') }/>
+            <img alt="" src={ require('./img/hotel-1.jpg') }/>
+            <img alt="" src={ require('./img/hotel-2.jpg') }/>
+            <img alt="" src={ require('./img/hotel-3.jpg') }/>
         </div>
     )
 }
