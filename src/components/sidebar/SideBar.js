abc3283b14453a943e69a149f8e41c5c183337b5
First Last <salmanashraf853@gmail.com>
2019-07-21 15:47:11 +0100

    a

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -3,7 +3,12 @@ import React from 'react'
 const SideBar = () => {
     return (
         <div className="side-bar">
-            {'side bar'}
+            <ul>
+                <li><button>HOME</button></li>
+                <li><button>FLIGHT</button></li>
+                <li><button>CAR RENTAL</button></li>
+                <li><button>TOUR</button></li>
+            </ul>
         </div>
     )
 }
