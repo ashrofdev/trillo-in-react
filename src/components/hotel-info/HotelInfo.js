d15fdc717afe1c1baa13f5adb7179e7fb4620dc4
First Last <salmanashraf853@gmail.com>
2019-07-23 18:23:10 +0100

    creating hotel info containers

--- src/components/hotel-info/HotelInfo.js
+++ src/components/hotel-info/HotelInfo.js
@@ -3,7 +3,18 @@ import React from 'react'
 const HotelInfo = () => {
     return (
         <div className="hotel-info">
-            {'Hotel Info'}
+            <div className="big-card">
+                {'big card'}
+            </div>
+            <div className="small-cards">
+                <div className="small-card">
+                    {'small card'}
+                </div>
+                <div className="small-card">
+                    {'small card'}
+                </div>
+                <button>Show all &rarr;</button>
+            </div>
         </div>
     )
 }
